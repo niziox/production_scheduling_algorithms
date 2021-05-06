9c830b9a56d6a1165be2c379d620bf7fbafa6fe2
Arkadiusz Kontek <arek8131@op.pl>
2021-05-06 16:37:12 +0200

    Create johnson_2_machines.py

---
+++ johnson_2_machines.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import numpy as np
+
+
+def johnson_2_machines(t_ij):
+    t_ij_opt = np.zeros(t_ij.shape)
+    left_threshold = 0
+    right_threshold = t_ij.shape[1] - 1
+    threshold = np.max(t_ij[1:, :]) + 1
+
+    while True:
+        min_tab = [np.min(t_ij[1:, :]), list(np.argwhere(t_ij[1:, :] == np.min(t_ij[1:, :])))]
+
+        for elem in min_tab[1]:
+            if elem[0] == 0:
+                t_ij_opt[:, left_threshold] = t_ij[:, elem[1]]
+                left_threshold += 1
+            elif elem[0] == 1:
+                t_ij_opt[:, right_threshold] = t_ij[:, elem[1]]
+                right_threshold -= 1
+            t_ij[0, elem[1]] = 0
+            t_ij[1:, elem[1]] = threshold
+
+        if np.all(t_ij[0, :] == 0):
+            return t_ij_opt
+
+
+if __name__ == '__main__':
+    m = np.array([[1, 2, 3, 4, 5, 6],
+                  [9, 6, 8, 7, 12, 3],
+                  [7, 3, 5, 10, 4, 7]])
+
+    print(johnson_2_machines(m))
